e69aaaca5da1801cb830cb88cf567771348a1363
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-05-02 10:39:22 +0530

    Security Bug Fixed

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+
 const errorMiddleware = require("./middleware/error");
 
+// Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
@@ -13,13 +15,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload())
+app.use(fileUpload());
 
-// Route imports
+// Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
-const payment = require("./routes/paymentRoute")
+const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
@@ -32,6 +34,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
-//  Middeleware for error
+// Middleware for Errors
 app.use(errorMiddleware);
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,37 @@
 const app = require("./app");
-const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
-// Handling uncayght Exception
-
+// Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to uncaught Exception`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
   process.exit(1);
 });
-// config
 
-dotenv.config({ path: "backend/config/config.env" });
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
-
+// Connecting to database
 connectDatabase();
-cloudinary.config({ 
+
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_API_SECRET
-})
-
-// const server = app.listen(port, () => {
-//   console.log(`listening on port : ${port}`);
-// });
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Unhandled promise rejection
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
+// Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to unhandled promise rejection`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
   server.close(() => {
     process.exit(1);
   });
